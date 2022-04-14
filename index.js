6f9ac247e083ab18d4e7a2f762a922facfe29bf4
Asadqayyummughal <asadqayyumbscs@gmail.com>
2022-04-15 01:30:29 +0500

    updated

--- index.js
+++ index.js
@@ -5,6 +5,12 @@
  let search_city;
  let list;
  const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+ //show cards variables
+ let country;
+ let weather;
+ let description;
+ let temp;
+ 
  //function call  api to get data
  async function getCityInfo() {
      search_city=document.getElementById('city').value;
@@ -12,35 +18,73 @@
      let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${search_city}&units=metric&appid=c73aa228bfba692462f96e89080aa39a`) 
      data = await response.json();
      list=data.list;
+     let obj=list[0]
       console.log('check data..------->',data);
-      showData();
+      showData(obj);
       makeCards();
   }
 // show dAta-
-function showData(){
+function showData(obj){
+  let day;
+  console.log('check obj...',obj);
   search_city=document.getElementById('city').value;
-  let country=data.city.country;
-  let weather=data.list[0].weather[0].main;
-  let description=data.list[0].weather[0].description;
-  let dayno=date.getDay();
-  let day=weekday[dayno];
+  country=data.city.country;
+  weather=obj.weather[0].main;
+   description=obj.weather[0].description;
+   console.log('check obj...',obj.day);
+  //  console.log('chek obj idde..',day in obj);
+   if(obj.day!=undefined)
+   {
+     day=obj.day;
+     console.log('check card dy..',day);
+     
+   }
+   else{
+    let dayno=new Date(obj.dt_txt).getDay();
+     day=weekday[dayno];
+   }
+
+  let icon=obj.weather[0].icon
   // check image function called
- let image=checkImage(weather,description)
- let temp=data.list[0].main.temp;
+ let image=checkImage(weather,description,icon)
+  temp=obj.main.temp;
+  let tmp_fhight=(9*temp+(32*5))/5
+  tmp_fhight= parseInt(tmp_fhight, 10)
+  tmp_fhight=tmp_fhight.toPrecision(2);
   let code=`<h2></h2>
       <h3>${search_city},${country}</h3>
-      <h4>${day}</h4>
+      <h4>${day}</h4>   
        <h5>${description}</h5>
-       <span><h1>${image}tempearature:${temp}</h1></span>`
+       <span><p> <img src="${image}" alt="">tempearature:${temp}&deg;<small>c</small>&nbsp;${tmp_fhight}&deg;F</p></span>`
   tem.innerHTML=code;
-  let pressure=data.list[0].main.pressure;
-  let humidity=data.list[0].main.humidity;
-  let wind_speed=data.list[0].wind.speed;
+  let pressure=obj.main.pressure;
+  let humidity=obj.main.humidity;
+  let wind_speed=obj.wind.speed;
   let code2=`  <h4>pressur:${pressure}hPa</h4>
                <h4>humidity:${humidity}%</h4>
                <h4>wind speed:${wind_speed}m/s</h4>
-       `
+            `
        Humidity.innerHTML=code2;
+    var xValues = [100,200,300,400,500];
+    var yValues = [5,8,11,2,5,];
+     new Chart("myChart",{
+     type:'line',
+    data:{
+      labels: xValues,
+      dataset:[{
+        backgroundColor: "rgba(0,0,0,1.0)",
+        borderColor: "rgba(0,0,0,0.1)",
+        data: yValues
+          } ]
+    },
+    Option:{
+      legend: {display: false},
+      scales: {
+      yAxes: [{ticks: {min: 6, max:16}}],
+    }
+    }
+ })
+
        //make cards
 }
 //function makeCards
@@ -49,6 +93,7 @@ function makeCards(){
   // let list=data.list;
   list=list.slice(0,5)
   let daysdiv=document.querySelector('.days');
+
   console.log('cheeck day div..>',daysdiv);
    let dayno=date.getDay();
     --dayno;
@@ -58,57 +103,69 @@ function makeCards(){
       dayno=0;
     }
     let day=weekday[dayno];
-    //call image check function
     let  maininfo=list[i].weather[0].main;
     console.log('check main....',maininfo);
     let description=list[i].weather[0].description;
     console.log('check description....',description);
-   let img=checkImage(maininfo,description);
-    let card=`<div class="card" id=card-${i}">
+    let icon=list[i].weather[0].icon;
+     //call image check function
+   let img=checkImage(maininfo,description,icon);
+    let card=`<div class="card" id=${i} onClick=editFunc(this)>
               <h2>${day}</h2>
-                 ${img}
-              <p>${list[i].main.temp_min}c${list[i].main.temp_max}c</p>
+              <img src="${img}" alt="">
+                
+              <p>${list[i].main.temp_min}&deg;<small>c</small>&nbsp;${list[i].main.temp_max}&deg;<small>c</small></p>
               </div>`
     daysdiv.innerHTML+=card;
   }
 
 }
   //check image function
- function checkImage(main,desc){
+ function checkImage(main,desc,icon){
    if(main=='Clouds' && desc=='broken clouds'||'scattered clouds '){
-    return `<img src="/images/sunandcloud.jpg" alt="">`
+    return `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${icon}.svg`
     
    } 
    else if(main=='Clouds' && desc=='all clouds'||'overcast clouds'){
-     return ` <img src="/images/allclouds.jpg" alt="">`
-
+     return `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${icon}.svg`
+     
    }
    else if(main=="Rain" && desc=='moderate rain'||'light rain'){
-          return `<img src="/images/rain.jpg" alt="">`
+          return `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${icon}svg`
     
    }
    else if(main='Clear' && desc=='clear sky'){
-    return `<img src="/images/sun.jpg" alt="">`
+    return `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${icon}.svg`
    }
    else if(main='Snow' && desc=='light snow'||'heavy snow')
    {
-      return `<img src="/images/snowfall.jpg" alt="">`
+      return `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${icon}.svg`
    }
    else if(main=='Thunderstorm'){
-       return `<img src="/images/thenderstorm.jpg" alt="">`
+       return `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${icon}.svg`
    }
    else{
-       return `<img src="/images/sunandcloud.jpg" alt="">`
+       return `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/{04n}.svg`
    }
           
    }
-   //Edit function
-  //  function editFunc(ev){
-  //    console.log('check id....',ev.target.getAttribute('id'));
-  //   //  console.log('check event.....',ev);
-  //   //  let id=ev.target.getAttribute('id');
-  //   //  console.log('check id...',id);
-  //  }
+  //  Edit function
+   function editFunc(ev){
+
+     console.log('check ev',ev);
+     let id=ev.getAttribute('id')
+     let getcard=list[id];
+     let parent=document.getElementById(id);
+     parent.style.height='';
+     parent.style.border='2px solid black'
+     parent.style.backgroundColor='#C0C0C0'
+     let day=parent.firstElementChild.textContent;
+     getcard['day']=day;
+     showData(getcard);
+      
+    
+   }
+
    
  
 
